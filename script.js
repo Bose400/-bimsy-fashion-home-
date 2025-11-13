2cd540dbb425378b939aff6193290c8e0e0b07cd
Abimbola <aiyedogbonabimbola3@gmail.com>
2025-11-13 19:15:28 +0100

    My first fashion site!

---
+++ script.js
@@ -0,0 +1,34 @@
+// Automatic slideshow
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("slide");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 3000);
+}
+
+// Smooth scroll and Paystack integration
+function payWithPaystack() {
+  let handler = PaystackPop.setup({
+    key: 'YOUR_PUBLIC_KEY', // replace with your Paystack public key
+    email: 'customer@email.com',
+    amount: 500000, // in kobo (₦5000)
+    currency: 'NGN',
+    callback: function (response) {
+      alert('Payment complete! Reference: ' + response.reference);
+    },
+    onClose: function () {
+      alert('Payment window closed.');
+    }
+  });
+  handler.openIframe();
+}
+
